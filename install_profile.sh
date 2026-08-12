d8936b073e7325fe7478f2397e98df05c98cc242
Manish <148604103+Manishkamat@users.noreply.github.com>
2026-08-12 18:58:38 +0530

    Fix script syntax and change ownership of config files
    Updated the script to fix syntax and ownership issues by removing 'sudo' for user-owned files.

--- install_profile.sh
+++ install_profile.sh
@@ -1,15 +1,22 @@
-# Fail on any command.
-set -eux pipefail
+#!/bin/bash
+
+# Fail on any command (Fixed syntax)
+set -euxo pipefail
+
+sudo apt update
+sudo apt install -y dconf-cli
 
 # Install plug-ins (you can git-pull to update them later).
 (cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting)
 (cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions)
 
 # Replace the configs with the saved one.
-sudo cp configs/.zshrc ~/.zshrc
+# (Removed 'sudo' so .zshrc is owned by YOU, not root)
+cp configs/.zshrc ~/.zshrc
 
 # Copy the modified Agnoster Theme
-sudo cp configs/pixegami-agnoster.zsh-theme ~/.oh-my-zsh/themes/pixegami-agnoster.zsh-theme
+# (Removed 'sudo' so the theme is owned by YOU, not root)
+cp configs/pixegami-agnoster.zsh-theme ~/.oh-my-zsh/themes/pixegami-agnoster.zsh-theme
 
 # Color Theme
 dconf load /org/gnome/terminal/legacy/profiles:/:fb358fc9-49ea-4252-ad34-1d25c649e633/ < configs/terminal_profile.dconf
@@ -20,9 +27,9 @@ old_list=$(dconf read /org/gnome/terminal/legacy/profiles:/list | tr -d "]")
 
 if [ -z "$old_list" ]
 then
-	front_list="["
+front_list="["
 else
-	front_list="$old_list, "
+front_list="$old_list, "
 fi
 
 new_list="$front_list'$add_list_id']"
