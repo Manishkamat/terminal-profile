107cfb9134197d154c0a2271ec3685910177bc29
Manish <148604103+Manishkamat@users.noreply.github.com>
2026-08-13 00:24:25 +0530

    Fix pipefail option and update Oh My Zsh installation
    Updated script to set pipefail option correctly and install Oh My Zsh in unattended mode.

--- install_terminal.sh
+++ install_terminal.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 # Fail on any command.
-set -eux pipefail
+set -euxo pipefail
 
 # Install ZSH
 sudo apt install -y git-core zsh curl
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Oh My Zsh (Unattended mode)
+RUNZSH=no CHSH=no UNATTENDED=yes sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
